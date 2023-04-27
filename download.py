523a65ae42d825666194ea3436de226a82bfa748
doron zarhi <doron88@gmail.com>
2023-04-27 08:28:00 +0300

    download: add packages

--- download.py
+++ download.py
@@ -6,7 +6,9 @@ import click
 import requests
 from plumbum import FG, local
 
-PACKAGES = ['pycharm-ce', 'emacs', 'sublime-text', 'rectangle', 'proxyman', 'flycut']
+PACKAGES = ['pycharm-ce', 'emacs', 'sublime-text', 'rectangle', 'proxyman', 'flycut', 'wireshark', 'google-chrome',
+            'firefox', 'drawio', 'audacity', 'microsoft-remote-desktop', 'vlc', 'cheatsheet', 'vmware-fusion',
+            'db-browser-for-sqlite', 'iterm2', 'docker', 'ghidra', 'charles']
 
 wget = local['wget']
 
